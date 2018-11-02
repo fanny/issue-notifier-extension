2397621832ffc68f28a5f4eb28ca8a8d22ed1d74
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-11-02 11:29:06 -0300

    Add firebase configurations

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,23 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({
+    apiKey: "AIzaSyA5j-DLK2oLZQBJNTt02WZl-y8WE62E-cI",
+    authDomain: "teste-69df9.firebaseapp.com",
+    databaseURL: "https://teste-69df9.firebaseio.com",
+    projectId: "teste-69df9",
+    storageBucket: "teste-69df9.appspot.com",
     messagingSenderId: "502490522309"
 });
 
 const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    const notificationTitle = payload.data.title;
+    const notificationOptions = {
+      body: payload.data.body,
+      icon: payload.data.icon
+    };
+    return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
+  
--- src/push-notifications.js
+++ src/push-notifications.js
@@ -2,14 +2,20 @@ import firebase from 'firebase';
 
 export const initializeFirebase = () => {
     firebase.initializeApp({
+        apiKey: "AIzaSyA5j-DLK2oLZQBJNTt02WZl-y8WE62E-cI",
+        authDomain: "teste-69df9.firebaseapp.com",
+        databaseURL: "https://teste-69df9.firebaseio.com",
+        projectId: "teste-69df9",
+        storageBucket: "teste-69df9.appspot.com",
         messagingSenderId: "502490522309"
     });
 
     navigator.serviceWorker
         .register('../firebase-messaging-sw.js')
-        .then((registration) =>{
+        .then((registration) => {
             firebase.messaging().useServiceWorker(registration);
-    })
+            console.log('registrando');
+        });
 }
 
 export const askForPermissionToReceiveNotifications = async () => {
@@ -17,6 +23,7 @@ export const askForPermissionToReceiveNotifications = async () => {
         const messaging = firebase.messaging();
         await messaging.requestPermission();
         const token = await messaging.getToken();
+        console.log('oii', token);
         console.log(`token do usuario: ${token}`);
 
         return token;
