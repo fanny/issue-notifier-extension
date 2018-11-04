8fe7047b121d615cbcde755d8f2e098a0131e6b3
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-11-04 10:39:59 -0300

    Add init chrome extension configurations

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -2,7 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: 'SENDER-ID'
+    apiKey: "AIzaSyA5j-DLK2oLZQBJNTt02WZl-y8WE62E-cI",
+    authDomain: "teste-69df9.firebaseapp.com",
+    databaseURL: "https://teste-69df9.firebaseio.com",
+    projectId: "teste-69df9",
+    storageBucket: "teste-69df9.appspot.com",
+    messagingSenderId: '502490522309'
 });
 
 const messaging = firebase.messaging();
